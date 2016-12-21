a75fa761ae9e56bb1ea6fb4666b9dd86fe08faee
Djordje Vujatovic <djordje_v@hotmail.com>
2016-12-20 17:26:12 -0800

    Added form, button to StorePicker.js

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component { 
     render(){
-
+      return(
+          //Inside JSX you can only ever return one parent element.
+        <form className="store-selector">
+           {/*This is how you write a comment inside JSX. Never put comments above parent element.*/}
+            <h2>Please Enter A Store</h2>
+            <input type="text" required placeholder ="Store Name"/>
+            <button type="submit">Visit Store</button>
+        </form>
+      )
     }    
 } 
 
