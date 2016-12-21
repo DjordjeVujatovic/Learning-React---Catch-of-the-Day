ad693ce831ed4869f3e50c99567a0d37fbe8e851
Djordje Vujatovic <djordje_v@hotmail.com>
2016-12-20 20:13:22 -0800

    Added Order.js and Inventory.js components.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom'; //importing render method from react-dom.
 import './css/style.css'; //Importing compiled style.css file
 import App from './components/App';
 import StorePicker from './components/StorePicker';//imports StorePicker component from StorePicker.js
+import Order from './components/Order';
+import Inventory from './components/Inventory';
 
 
 
