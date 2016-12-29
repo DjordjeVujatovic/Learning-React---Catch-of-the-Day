c500949d4e59098f1f50d6cc6e6394412090e960
Djordje Vujatovic <djordje_v@hotmail.com>
2016-12-28 17:24:53 -0800

    Added remove button to order form.

--- src/components/App.js
+++ src/components/App.js
@@ -12,9 +12,12 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
-    this.updateFish= this.updateFish.bind(this);
-    this.loadSamples= this.loadSamples.bind(this);
+    this.removeFish = this.removeFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.removeFromOrder = this.removeFromOrder.bind(this);
+   
     //Getting initial state
     this.state= {
       fishes: {},
@@ -63,6 +66,12 @@ class App extends React.Component {
     fishes[key] = updatedFish;
     this.setState({fishes});
   }
+  removeFish(key) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = null;
+    this.setState({fishes});
+  }
+
   loadSamples(){
     this.setState({
       fishes: sampleFishes
@@ -77,6 +86,11 @@ order[key] = order[key] + 1 || 1;
 //Update our state
 this.setState({order});
 }
+removeFromOrder(key) {
+    const order = {...this.state.order};
+    delete order[key];
+    this.setState({order});
+  }
     render(){
       return(
         <div className="catch-of-the-day">
@@ -93,9 +107,11 @@ this.setState({order});
               fishes={this.state.fishes} 
               order={this.state.order}
               params={this.props.params}
+              removeFromOrder={this.removeFromOrder}
               />
             <Inventory 
               addFish={this.addFish} 
+              removeFish={this.removeFish}
               loadSamples={this.loadSamples} 
               fishes={this.state.fishes}
               updateFish={this.updateFish}
