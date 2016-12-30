9ea8610b55d2902b7f20d526dca729df266cc571
Djordje Vujatovic <djordje_v@hotmail.com>
2016-12-29 16:18:45 -0800

    Added animations to order form. Validated components with proptypes.

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -33,7 +33,7 @@ class Inventory extends React.Component {
          </textarea>
 
          <input type="text" name="image" value={fish.image} placeholder="Fish Image" onChange={(e)=>this.handleChange(e, key)}/>
-         <button onClick ={()=>this.props.removeFish(key)}>Remove Fish</button>
+         <button onClick={()=>this.props.removeFish(key)}>Remove Fish</button>
        </div>
      )
    }
@@ -49,5 +49,11 @@ class Inventory extends React.Component {
       )
     }    
 } 
-
+Inventory.propTypes = {
+  fishes: React.PropTypes.object.isRequired,
+  updateFish: React.PropTypes.func.isRequired,
+  removeFish: React.PropTypes.func.isRequired,
+  addFish: React.PropTypes.func.isRequired,
+  loadSampleS: React.PropTypes.func.isRequired
+}
 export default Inventory;
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { formatPrice } from '../helpers';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 class Order extends React.Component { 
     constructor (){
         super();
@@ -15,7 +16,18 @@ class Order extends React.Component {
      }
      return (
          <li key={key}>
-            <span>{count}lbs {fish.name} {removeButton}</span>
+            <span>
+            <CSSTransitionGroup
+              component="span"
+              className="count"
+              transitionName="count"
+              transitionEnterTimeout={250}
+              transitionLeaveTimeout={250}
+            >
+                <span key={count}>{count}</span>
+            </CSSTransitionGroup>
+            lbs {fish.name} {removeButton}
+            </span>
             <span className="price">{formatPrice(count * fish.price)}</span>
          </li>
      )
@@ -36,7 +48,15 @@ class Order extends React.Component {
 
          <div className="order-wrap">
             <h2> Your Order </h2>
-            <ul className="order">
+            <CSSTransitionGroup 
+
+                className="order" 
+                component="ul"
+                transitionName="order"
+                transitionEnterTimeout={500}
+                transitionLeaveTimeout={500}
+    
+                >
              {orderIds.map(this.renderOrder)}
               <li className="total">
                 <strong>Total:</strong>
@@ -44,11 +64,15 @@ class Order extends React.Component {
             
                  
               </li>
-            </ul>
+            </CSSTransitionGroup>
 
          </div>
       )
     }    
 } 
-
+Order.propTypes = {
+ fishes: React.PropTypes.object.isRequired,
+ order: React.PropTypes.object.isRequired,
+ removeFromOrder: React.PropTypes.func.isRequired
+}
 export default Order;
